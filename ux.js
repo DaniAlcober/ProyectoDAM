0d8c6fd6713e4aa21410a16d0f8648b276270ec7
Daniel Alcober <daniel.alcober.96@gmail.com>
2023-05-09 19:05:35 +0200

    first commit

---
+++ ux.js
@@ -0,0 +1,17 @@
+// Show/Hide Menu
+//var hamburger = document.querySelector(".hamburger");
+//hamburger.addEventListener("click", function () {
+//    document.querySelector("body").classList.toggle("active");
+//})
+
+
+// Make clicked tab active
+var tab = document.querySelectorAll("a");
+tab.forEach(t => {
+    t.addEventListener('click', () => {
+        var tabActive = document.querySelector("a.active");
+        tabActive.classList.remove('active');
+        t.classList.add('active');
+    })
+    
+});
