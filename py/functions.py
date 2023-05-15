21def4f1431a0be6459e2ddf3c69167e02c6b74f
Daniel Alcober <daniel.alcober.96@gmail.com>
2023-05-15 20:26:07 +0200

    Añadir a functions.py y crear functions.json

--- py/functions.py
+++ py/functions.py
@@ -9,3 +9,18 @@ def multiplica(a, b):
 
 def divide(a, b):
     return a / b
+
+def cuentaString(str, char):
+   return str.count(char)
+
+def mayusculas(str):
+    return str.upper()
+
+def minusculas(str):
+    return str.lower()
+
+def mayusculasPrimera(str):
+    return str.capitalize()
+
+
+
